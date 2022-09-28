abf67f2b359ea099fa770385e8f2e1d5425110d4
AnuPratap10 <anupratapsingh49@gmail.com>
2022-09-29 02:30:24 +0530

    work panding on navbar

---
+++ calendly-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MainFile from './Component/MainFile';
+
+function App() {
+  return (
+    <div className="App">
+      <MainFile/>
+    </div>
+  );
+}
+
+export default App;
